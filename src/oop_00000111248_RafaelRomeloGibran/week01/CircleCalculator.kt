6bc76450bd595028eabdef285950b2872497551f
unknown <rafaelromelogibran400@gmail.com>
2026-02-06 10:46:57 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/oop_00000111248_RafaelRomeloGibran/week01/CircleCalculator.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week01/CircleCalculator.kt
@@ -10,11 +10,8 @@ fun main(args: Array<String>) {
     println("Radius: $radius, Area: $area")
     // Logic check
     checkSize(area)
+
+    println("${checkSize(area)}")it
 }
-fun checkSize(area) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+
+fun checkSize(area: Double) =if (area > 100) "This is a Big Circle" else "This is a Small Circle"
