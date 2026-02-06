dd6721a472eaa36815cff2619046c43c4047c50b
unknown <rafaelromelogibran400@gmail.com>
2026-02-06 11:22:47 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000111248_RafaelRomeloGibran/week01/GameStore.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week01/GameStore.kt
@@ -3,10 +3,21 @@ package oop_00000111248_RafaelRomeloGibran.week01
 import kotlin.math.roundToInt
 
 fun main(){
-    val gameTitle: String
-    val price: Int
+    val userNote: String? = null
+    val gameTitle: String = "Deadpool"
+    val price: Int = 1000
 
+    val finalPrice: Int = calculateDiscount(price)
+    printReceipt(gameTitle, finalPrice)
+
+    println("Harga Awal: $price")
 
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) price - (price * 0.2).roundToInt() else price - (price * 0.1).roundToInt()
+
+fun printReceipt(title: String, finalPrice: Int) {
+
+    println("judul: $title")
+    println("Harga Akhir: $finalPrice")
+}
