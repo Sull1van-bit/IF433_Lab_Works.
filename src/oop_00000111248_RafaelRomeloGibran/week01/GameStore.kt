134d8f17d5d56f262163eaa0aa7dc5db5f6e6bca
unknown <rafaelromelogibran400@gmail.com>
2026-02-06 10:58:04 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000111248_RafaelRomeloGibran/week01/GameStore.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week01/GameStore.kt
@@ -1,6 +1,12 @@
 package oop_00000111248_RafaelRomeloGibran.week01
 
+import kotlin.math.roundToInt
+
 fun main(){
     val gameTitle: String
     val price: Int
+
+
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) price - (price * 0.2).roundToInt() else price - (price * 0.1).roundToInt()
