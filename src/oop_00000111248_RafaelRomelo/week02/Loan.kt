33959bb0bac6446c697dc354ab5bcf5357086142
unknown <rafaelromelogibran400@gmail.com>
2026-02-15 13:34:37 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000111248_RafaelRomelo/week02/Loan.kt
@@ -0,0 +1,12 @@
+package oop_00000111248_RafaelRomelo.week02
+
+class Loan(val bookTitle: String, val borrower: String, val loanDuration: Int = 1) {
+
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            return 0
+        }
+    }
+}
