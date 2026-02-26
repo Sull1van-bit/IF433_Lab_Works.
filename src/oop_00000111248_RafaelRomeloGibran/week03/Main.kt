72e0c2b02ebba0a5e94cd3fa76d134165d2359a1
unknown <rafaelromelogibran400@gmail.com>
2026-02-26 23:50:33 +0700

    week03: cleanup code

--- src/oop_00000111248_RafaelRomeloGibran/week03/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week03/Main.kt
@@ -4,13 +4,11 @@ fun main() {
     val e = Employee("Budi")
 
     // 1. Test validasi salary
-    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
     // 2. Test encapsulation
     e.increasePerformance()
-    //e.performanceRating = 5
 
     // 3. Test computated property
     println("Pajak yang harus dibayar: ${e.tax}")
