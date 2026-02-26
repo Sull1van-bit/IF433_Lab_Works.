9bc7b2fb8db99bd2c68ae5e56eb8a64fa257f202
unknown <rafaelromelogibran400@gmail.com>
2026-02-26 23:38:10 +0700

    week03: simulate stack overflow error in setter

---
+++ src/oop_00000111248_RafaelRomeloGibran/week03/Employee.kt
@@ -0,0 +1,9 @@
+package oop_00000111248_RafaelRomeloGibran.week03
+
+class Employee(val name: String) {
+    var salary: Int = 0
+    set(value) {
+        println("Mencoba set gaji ke: $value")
+        this.salary = value
+    }
+}
--- src/oop_00000111248_RafaelRomeloGibran/week03/Employee/Employee.kt
+++
@@ -1,5 +0,0 @@
-package oop_00000111248_RafaelRomeloGibran.week03.Employee
-
-class Employee(val name: String) {
-    var salary: Int = 0
-}
