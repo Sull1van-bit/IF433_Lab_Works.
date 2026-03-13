60c02af5dc1c34e8548732b734e4a5f17ddce569
unknown <rafaelromelogibran400@gmail.com>
2026-03-13 11:07:16 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000111248_RafaelRomeloGibran/week05/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week05/Main.kt
@@ -1,8 +1,12 @@
 package oop_00000111248_RafaelRomeloGibran.week05
 
 fun main() {
-    val math1 = MathHelper()
-    println("Luas Persegi: ${math1.hitungLuas(5)}")
-    println("Luas Persegi Panjang: ${math1.hitungLuas(5, 3)}")
-    println("Luas Lingkaran: ${math1.hitungLuas(7.0)}")
+    val wallet = EWallet("Rafael", 50000.0)
+    val card = CreditCard(100000.0, 0.0, "Rafael")
+
+    val listPaymentMethod: List<PaymentMethod> = listOf(wallet, card)
+
+    for (payment in listPaymentMethod) {
+        payment.processPayment(75000.0)
+    }
 }
