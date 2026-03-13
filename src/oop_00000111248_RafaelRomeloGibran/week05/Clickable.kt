e987d0594aadf9408522e84075a20d4de70a5a67
unknown <rafaelromelogibran400@gmail.com>
2026-03-13 11:13:03 +0700

    week06: (trial) create Clickable interface with backing field error

--- src/oop_00000111248_RafaelRomeloGibran/week05/Clickable.kt
+++
@@ -1,7 +0,0 @@
-package oop_00000111248_RafaelRomeloGibran.week05
-
-interface Clickable {
-    val name: String
-
-    fun click()
-}
---
+++ src/oop_00000111248_RafaelRomeloGibran/week06/Clickable.kt
@@ -0,0 +1,7 @@
+package oop_00000111248_RafaelRomeloGibran.week06
+
+interface Clickable {
+    val name: String = "Tombol Raahasi"
+
+    fun click()
+}
