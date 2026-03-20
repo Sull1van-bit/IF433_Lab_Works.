ac31ae39e03b8a500ef8179385141c96b51b4757
unknown <rafaelromelogibran400@gmail.com>
2026-03-20 19:26:43 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000111248_RafaelRomeloGibran/week06/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week06/Main.kt
@@ -1,21 +1,7 @@
 package oop_00000111248_RafaelRomeloGibran.week06
 
-fun processCheckout(method: PaymentMethod, amount: Double) {
-    println("Checkout")
-    method.pay(amount)
-}
-
 fun main() {
-    val myWatch = Smartwatch()
-    myWatch.showTime()
-
-    val myPhone = Smartphone()
-    myPhone.turnOn()
-
-    val pay1 = Gopay()
-    val pay2 = CreditCard()
-
-    println("==== TESTING CHECKOUT ====")
-    processCheckout(pay1, 50000.0)
-    processCheckout(pay2, 150000.0)
+    val lamp = SmartLamp(id = "L1", name = "Ruang Tamu")
+    val speaker = SmartSpeaker(id = "S1", name = "Google Nest Dapur")
+    val cctv = SmartCCTV(id = "C1", name = "Garasi")
 }
