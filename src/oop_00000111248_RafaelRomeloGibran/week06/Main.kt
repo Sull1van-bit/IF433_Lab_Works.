bc7dd78a7447db77e557e9dc7c56d50832dbdf13
unknown <rafaelromelogibran400@gmail.com>
2026-03-20 19:27:19 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000111248_RafaelRomeloGibran/week06/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week06/Main.kt
@@ -4,4 +4,9 @@ fun main() {
     val lamp = SmartLamp(id = "L1", name = "Ruang Tamu")
     val speaker = SmartSpeaker(id = "S1", name = "Google Nest Dapur")
     val cctv = SmartCCTV(id = "C1", name = "Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
 }
