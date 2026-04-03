91f3acc434e4d04d28e08c4cd84966b094c0b2c3
unknown <rafaelromelogibran400@gmail.com>
2026-04-03 17:13:11 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
@@ -2,8 +2,13 @@ package oop_00000111248_RafaelRomeloGibran.week07
 
 fun main() {
     println("test regular class")
-    val reg1 = DataUser("Rafael", 20)
+    val data1 = DataUser("Rafael", 20)
     val reg2 = DataUser("Rafael", 20)
-    println(reg1)
-    println("sama? ${reg1 == reg2}")
+    println(data1)
+    println("sama? ${data1 == reg2}")
+
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
 }
