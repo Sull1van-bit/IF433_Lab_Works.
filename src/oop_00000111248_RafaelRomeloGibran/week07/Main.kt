08fd271bdd4a2d1da6b062743ff46333929959a1
unknown <rafaelromelogibran400@gmail.com>
2026-04-03 17:11:28 +0700

    week07: implement data class for structural equality

--- src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
@@ -2,8 +2,8 @@ package oop_00000111248_RafaelRomeloGibran.week07
 
 fun main() {
     println("test regular class")
-    val reg1 = RegularUser("Rafael", 20)
-    val reg2 = RegularUser("Timmy", 19)
+    val reg1 = DataUser("Rafael", 20)
+    val reg2 = DataUser("Rafael", 20)
     println(reg1)
     println("sama? ${reg1 == reg2}")
 }
