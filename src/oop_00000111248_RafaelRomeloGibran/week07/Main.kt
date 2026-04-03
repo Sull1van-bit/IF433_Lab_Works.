c89125757fc96f0dba7c8db1311c1bbeab6223a2
unknown <rafaelromelogibran400@gmail.com>
2026-04-03 17:16:35 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
@@ -1,14 +1,10 @@
 package oop_00000111248_RafaelRomeloGibran.week07
 
 fun main() {
-    println("test regular class")
-    val data1 = DataUser("Rafael", 20)
-    val reg2 = DataUser("Rafael", 20)
-    println(data1)
-    println("sama? ${data1 == reg2}")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
-    val data3 = data1.copy(age = 23)
-    println("Hasil Copy: $data3")
-    val (userName, userAge) = data1
-    println("Destructured: $userName berumur $userAge")
+    val uiMessage = when (response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    }
 }
