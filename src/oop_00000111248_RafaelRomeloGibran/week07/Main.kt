d28e489aaf22a808d25333ff2355c11cf8dedbcf
unknown <rafaelromelogibran400@gmail.com>
2026-04-03 17:06:04 +0700

    "week07: (trial) demonstrate regular class equality failure"

--- src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week07/Main.kt
@@ -1,11 +1,9 @@
 package oop_00000111248_RafaelRomeloGibran.week07
 
-fun Main() {
-    println("test singleton")
-    println("Status: ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
-
-    println("Test object")
-    val client = NetworkClient.createClient()
-    client.connect()
+fun main() {
+    println("test regular class")
+    val reg1 = RegularUser("Rafael", 20)
+    val reg2 = RegularUser("Timmy", 19)
+    println(reg1)
+    println("sama? ${reg1 == reg2}")
 }
