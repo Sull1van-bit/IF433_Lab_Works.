abea539e3edc7cff7dfbe2579ca281695d992313
unknown <rafaelromelogibran400@gmail.com>
2026-04-17 11:17:26 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000111248_RafaelRomeloGibran/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000111248_RafaelRomeloGibran.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
+
