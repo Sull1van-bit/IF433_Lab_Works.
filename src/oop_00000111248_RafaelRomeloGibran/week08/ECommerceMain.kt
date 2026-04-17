d695f0fcb8311364f3f1def6cf8948b696683fb1
unknown <rafaelromelogibran400@gmail.com>
2026-04-17 11:30:32 +0700

    week08: (task) process mixed raw JSON list using as?

--- src/oop_00000111248_RafaelRomeloGibran/week08/ECommerceMain.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week08/ECommerceMain.kt
@@ -4,9 +4,9 @@ fun main() {
     val rawApiData: List<Map<String, Any?>> = listOf(
         mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
         mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
-        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Corrupted warranty
-        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
-        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"),
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"),
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD")
     )
 
     val parser = ApiParser()
@@ -21,4 +21,5 @@ fun main() {
             println("Warning: Corrupted Data - ${e.message}")
         }
     }
+    //success
 }
