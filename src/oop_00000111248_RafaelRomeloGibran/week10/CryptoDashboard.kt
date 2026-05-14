e9682650818361ca275888f324a9e845e3e98201
unknown <rafaelromelogibran400@gmail.com>
2026-05-14 23:34:58 +0700

    week10: (task) populate coin repository

--- src/oop_00000111248_RafaelRomeloGibran/week10/CryptoDashboard.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000111248_RafaelRomeloGibran.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 15.0))
+    coinRepo.add(Coin("USDT", 250.0))
 }
