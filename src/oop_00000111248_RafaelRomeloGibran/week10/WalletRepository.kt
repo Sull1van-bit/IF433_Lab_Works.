2419613ccd8fa06827e3f5758834c73925974d9e
unknown <rafaelromelogibran400@gmail.com>
2026-05-14 23:32:09 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000111248_RafaelRomeloGibran/week10/WalletRepository.kt
@@ -0,0 +1,17 @@
+package oop_00000111248_RafaelRomeloGibran.week10
+
+class WalletRepository<T : Any> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+    fun find(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
+    }
+}
