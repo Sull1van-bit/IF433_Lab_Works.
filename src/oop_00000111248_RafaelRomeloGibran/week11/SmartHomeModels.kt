eb7365ef45f090bdadd4b077a88770d94b9890bd
unknown <rafaelromelogibran400@gmail.com>
2026-05-15 00:06:14 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000111248_RafaelRomeloGibran/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000111248_RafaelRomeloGibran.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
