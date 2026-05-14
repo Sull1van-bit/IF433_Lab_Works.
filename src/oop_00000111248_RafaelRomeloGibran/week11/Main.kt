55425f69a99a6d6e86662651c46951b0f61f9ef3
unknown <rafaelromelogibran400@gmail.com>
2026-05-14 23:59:00 +0700

    week11: implement let function for null safety

--- src/oop_00000111248_RafaelRomeloGibran/week11/Main.kt
+++ src/oop_00000111248_RafaelRomeloGibran/week11/Main.kt
@@ -6,4 +6,12 @@ fun main() {
     println("Hi".repeatTimes(3))
     val text: String? = null
     println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Panjang nama: $length")
 }
